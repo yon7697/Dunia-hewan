bd024bc5a9be4aab332f3848d1a4484f40279328
yon7697 <lancelotid223@gmail.com>
2026-09-16 21:06:51 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'dunia-hewan-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
